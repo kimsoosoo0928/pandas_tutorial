0198b7473b30045ff7cdbec3783de65bb6182b9c
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-06-23 19:31:19 +0900

    pandas class

---
+++ pet.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from icecream import ic
+
+if __name__ == '__main__':
+    menu = input('##')
+
+    def quiz_2():
+        pass
+
+
+    def quiz_2():
+        pass
+
+
+    def quiz_2():
+        pass
+
+
+    def quiz_2():
+        pass
+
+
+    def quiz_2():
+        pass
+
+    def quiz_2():
+        pass
+    def quiz_2():
+
+    while 1:
+        menu = input(menu)
